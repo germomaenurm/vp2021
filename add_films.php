e3383de8bf4850e9051a69a8fff2b1d4ef2ba628
Germo Mäenurm <germomaenurm@users.noreply.github.com>
2021-09-27 20:57:02 +0300

    kodus tehtud 27/09

--- add_films.php
+++ add_films.php
@@ -7,9 +7,47 @@
 	//kas klikiti submit nuppu
 		if(isset($_POST["film_submit"])) {
 			if(!empty($_POST["title_input"]) and !empty($_POST["genre_input"]) and !empty($_POST["studio_input"]) and !empty($_POST["director_input"])) {
-				$film_store_notice = store_film($_POST["title_input"], $_POST["year_input"], $_POST["duration_input"], $_POST["genre_input"], $_POST["studio_input"], $_POST["director_input"]);
+				$film_store_notice = store_film(test_input($_POST["title_input"]), test_input($_POST["year_input"]), test_input($_POST["duration_input"]), test_input($_POST["genre_input"]), test_input($_POST["studio_input"]), test_input($_POST["director_input"]));
 			} else {
 				$film_store_notice = "Osa andmeid on puudu!";
+			}
+		}
+		
+		$title_error = null;
+		$genre_error = null;
+		$studio_error = null;
+		$director_error = null;
+	
+	//teen iga kohta kontrolli
+		if(isset($_POST["film_submit"])) {
+			if(empty($_POST["title_input"])) {
+				$title_error = "Kirjuta pealkiri";
+			} else {
+				$title_error;
+			}
+		}
+		
+		if(isset($_POST["film_submit"])) {
+			if(empty($_POST["genre_input"])) {
+				$genre_error = "Kirjuta žanr";
+			} else {
+				$genre_error;
+			}
+		}
+		
+		if(isset($_POST["film_submit"])) {
+			if(empty($_POST["studio_input"])) {
+				$studio_error = "Kirjuta tootja";
+			} else {
+				$studio_error;
+			}
+		}
+		
+		if(isset($_POST["film_submit"])) {
+			if(empty($_POST["director_input"])) {
+				$director_error = "Kirjuta lavastaja";
+			} else {
+				$director_error;
 			}
 		}
 		
@@ -28,7 +66,8 @@
 		<h2>Eesti filmid</h2>
 		<form method="POST">
 			<label for="title_input">Filmi pealkiri: </label>
-			<input type="text" name="title_input" id="title_input" placeholder="pealkiri">
+			<input type="text" name="title_input" id="title_input" placeholder="pealkiri"> 
+			<span><?php echo $title_error; ?></span>
 			<br>
 			<label for="year_input">Filmi valmimisaasta: </label>
 			<input type="number" name="year_input" id="year_input" value="<?php echo date("Y"); ?>" min="1912">
@@ -38,12 +77,15 @@
 			<br>
 			<label for="genre_input">Filmi žanr: </label>
 			<input type="text" name="genre_input" id="genre_input" placeholder="žanr">
+			<span><?php echo $genre_error; ?></span>
 			<br>
 			<label for="studio_input">Filmi tootja: </label>
 			<input type="text" name="studio_input" id="studio_input" placeholder="tootja">
+			<span><?php echo $studio_error; ?></span>
 			<br>
 			<label for="director_input">filmi lavastaja: </label>
 			<input type="text" name="director_input" id="director_input" placeholder="lavastaja">
+			<span><?php echo $director_error; ?></span>
 			<br>
 			<input type="submit" name="film_submit" value="Salvesta">
 			
